6a9354e1b8f16bd76a58bf3a172afd4f60a7a9f8
Thiago <thiago099carvalho@gmail.com>
2022-08-17 21:45:09 -0300

    fix: main page resposivity

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 const Main = () => {
   return(
     <div className="main-background h-screen overflow-auto flex justify-center">
-      <div className="items-center bg-contain flex justify-items-center main-background bg-[url('/assets/NiseOpacity.svg')] bg-no-repeat bg-center sm:bg-auto">
+      <div className="bg-contain flex justify-items-center main-background bg-[url('/assets/NiseOpacity.svg')] bg-no-repeat bg-center sm:bg-auto sm:items-center">
         <div className="md:px-20 w-full">
           <h1 className="pb-10 py-10 text-center">Cartas para Nise</h1>
           <p className="px-10 pb-10 text-left font-secondary font-light text-2xl">E assim, através do tempo e dos lugares, você foi fascinando grandes, pequenos, pequeníssimos. Diante da correria dessa imensa trajetória, trouxeram-te exatamente aqui na minha interposição de palavras e busca de sentidos que visam dar voz as angústias, reflexões e inquietações da minha nova experiência. Sou Beatriz, assistente social e residente em saúde mental, e direciono todas as minhas cartas a tu, Nise, pois busco seus olhos e coração atentos a minha jornada audaciosa. Tenha uma ótima leitura!</p>
